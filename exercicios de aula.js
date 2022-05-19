d90493b74f1ce6b1ec60d56f1643f29b06153783
venous666 <99412095+venous666@users.noreply.github.com>
2022-05-19 12:43:33 -0300

    Update exercicios de aula.js

--- exercicios de aula.js
+++ exercicios de aula.js
@@ -1,11 +1,65 @@
 //let nomes = ["Jonas,Venus,Jaiza,Caire,Bernardo"]
 //console.log(nomes);
 
-let salarios = ["R$ 5000,R$ 2000,R$ 3000,R$ 8000,R$ 2000,R$ 3000,R$ 7000,R$ 2500"]
-console.log(salarios);
+//let salarios = ["R$ 5000,R$ 2000,R$ 3000,R$ 8000,R$ 2000,R$ 3000,R$ 7000,R$ 2500"]
+//console.log(salarios);
 
-let boolean = [2 < 3, 6 > 7, 3 > 7]
-    console.log(boolean);
+//let boolean = [2 < 3, 6 > 7, 3 > 7]
+    //console.log(boolean);
     
-    
+//     let matriz = [[8, 7, 2]]
+//     let preço = ["69"]
+//    for (i in matriz){
+//        matriz [i][0]*= preço
+
+//    }
+//    for (i in matriz){
+//     matriz [i][1]*= preço
+
+// }
+// for (i in matriz){
+//     matriz [i][2]*= preço
+
+// 
+//        console.table(matriz)
+
+// Q5pre = [150,27.6,44.6];
+// Q5 = [];
+// for(i of Q5pre)
+//     Q5.push(i)
+//     Desconto = i * (95/100)
+//     Q5.push(Desconto);
+//     console.table(Q5)
+//5
+
+// let matriz = [[5, 5, 3],[2,4,3],[2,8,9]]
+// for(let i in matriz){
+//     console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|")
+// }
+// console.log('MATRIZ DOBRADA')
+// for (let i in matriz){
+  
+//     matriz[i][i] *= 2
+//     console.log("|", matriz[i][0], matriz[i][1], matriz[i][2], "|") 
+// }
+//6
+//7
+
+// function random(max,min){
+//     n = Math.floor(Math.random()* (max - min) + min);
+//     return n;
+// }
+
+// q7 = []
+// for(i = 0; i < 2; i++){
+//     arr = []
+//     q7.push(arr);
+//     for(ii = 0; ii < 2; ii++){
+//         q7[i].push(random(9,0));
+//     }
+
+// }
+
+// det = (q7[0][0]* q7[1][1]) - (q7[0][1] * q7[1][0]);
+// console.log(q7, det);
 
